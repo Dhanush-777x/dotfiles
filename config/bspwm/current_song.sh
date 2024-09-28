5af5c1bccf2aba1611a482bca29e6d540817e04a
Dhanush-777x <dhanushmani1504@gmail.com>
2024-09-29 02:06:07 +0530

    fix: current song title overflow

--- config/bspwm/current_song.sh
+++ config/bspwm/current_song.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
-# Get the currently playing song
-song=$(playerctl metadata title)
+# Maximum length of the song title
+max_length=30
+
+# List of offline audio players to check
+offline_players=("spotify" "rhythmbox" "audacious" "mpd" "deadbeef")
+
+# Get the currently active player
+active_player=$(playerctl -l | grep -E "$(IFS=\|; echo "${offline_players[*]}")" | head -n 1)
+
+# Get the title of the currently playing track
+if [ -n "$active_player" ]; then
+    title=$(playerctl -p "$active_player" metadata title 2>/dev/null)
+else
+    title=""
+fi
+
+# Truncate the song title if it exceeds max_length
+if [[ -n "$title" && ${#title} -gt $max_length ]]; then
+    title="${title:0:max_length}..."
+fi
 
 # If there is no song playing, display "No Music"
-if [[ -z "$song" ]]; then
+if [[ -z "$title" ]]; then
     echo "No Music"
 else
-    echo "$song"
+    echo "$title"
 fi
