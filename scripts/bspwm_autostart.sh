66260359021f0ca0d508126cbecd11cfb8cf1f31
dhanushsm <dhanushmani1504@gmail.com>
2025-02-10 00:00:01 +0530

    Updated workspace setup script

--- scripts/bspwm_autostart.sh
+++ scripts/bspwm_autostart.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
+# Show notification using dunstify
+dunstify "Welcome Back Dhanush!" "Setting Up Your Workspace. Please Wait..." -t 5000 &
+
 # Wait for BSPWM to initialize completely
 sleep 2
 
-# Open Brave in the 1st workspace
+# Open Personal Brave in the 1st workspace
 brave &
-sleep 1
+sleep 1  
 bspc node -d 1
 
-# Open Terminal with Tmux in the 2nd workspace
-kitty -e tmux -u &
-sleep 1
+# Open Work Brave in the 1st workspace
+brave &
+sleep 1  
 bspc node -d 2
+
+# Start two tmux sessions
+tmux -u new-session -d -s Term        # Create Term session
+tmux -u new-session -d -s nvim        # Create nvim session
+
+# Open nvim inside the "nvim" session
+tmux -u send-keys -t nvim "nvim" Enter
+tmux -u detach -s nvim 
+
+# Open terminal in workspace 2 with the "Term" session
+kitty --class "Term" -e tmux -u attach-session -t Term &
+sleep 1  # Wait for the terminal to launch
+
+# Move it to workspace 2
+bspc node -d 3
+sleep 1  
 
 # Open Whatsapp in the 6th workspace
 gtk-launch brave-hnpfjngllnobngcgfapefoaidbinmjnm-Default.desktop &
@@ -19,5 +38,7 @@ sleep 1
 bspc node -d 6
 
 # Return focus to the first workspace
-bspc desktop -f 1
+bspc desktop -f 2
 
+#Ready to go notification
+dunstify "Setting Up Done" "You are ready to go!" -t 5000 &
